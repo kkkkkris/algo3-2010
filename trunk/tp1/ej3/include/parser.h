2a4de0c38eeaf3aed7f5d96e21c1f782da99141b
joaquin.garavaglia <joaquin.garavaglia@002c8353-63d8-11a8-4f12-8c9ae8348763>
2010-04-04 21:45:21 +0000

    Agregado ej3 .

---
+++ trunk/tp1/ej3/include/parser.h
@@ -0,0 +1,12 @@
+#ifndef __PARSER__
+#define __PARSER__
+
+#include <list>
+#include <vector>
+#define Planilla vector<int>
+
+using namespace std;
+
+list<Planilla*> parsearInstancias(string entrada);
+
+#endif //__PARSER__
---
+++ trunk/tp1/ej3/src/parser.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include "parser.h"
+
+using namespace std;
+
+int parsearHora(string hora);
+
+list<Planilla*> parsearInstancias(string entrada){
+    list<Planilla*> planillas;
+    Planilla planilla;
+
+    stringstream in(entrada);
+    int horas_size=0,pos=0,p=1;
+
+    while(!in.eof() && horas_size != -1){
+        string line;
+        getline(in,line);
+        if(pos < horas_size){                   //parsea las horas ,las transforma en int [0-86399] y las pone en planilla
+        planilla.push_back(parsearHora(line));
+        pos++;
+        }else{                                  //cierra y guarda anterior instancia si existe e inicia proxima instancia si no se termino
+            if(!planilla.empty()){
+                cout<<"Planilla #"<<p<<": [";
+                for(Planilla::iterator it =planilla.begin();it != planilla.end();it++){
+                    cout<<*it<<",";
+                }
+                cout<<"]"<<endl;
+                p++;
+                vector<int>* pl = new vector<int>(planilla);
+                planillas.push_back(pl);
+            }
+            horas_size=2*(atoi(line.c_str()));  //dame tam de la prox planilla
+            if(horas_size !=-1){                //si no es el final reseteo planilla
+                pos=0;
+                planilla.clear();
+            }
+        }
+    }
+    return planillas;
+}
+
+int parsearHora(string hora) {
+    int res;
+    res = atoi(hora.substr(0,2).c_str())*3600 ;   //c_str() convierte string a char[]
+    res+= atoi(hora.substr(3,2).c_str())*60;
+    res+= atoi(hora.substr(6,2).c_str());
+    return res;
+}
