d09be46e5488a12e0c5c703ed12efebccf89b804
santiago.pivetta <santiago.pivetta@002c8353-63d8-11a8-4f12-8c9ae8348763>
2010-04-09 19:58:43 +0000

    Agregue un par de chequeos para contestar el problema sin tener que recorrer el grafo y armar el ciclo. Ambos chequeos se realizan en el constructor. Mientras se carga cada nodo en la estructura, se verifica por un lado que todos tengan mas de 1 amiga. Si existe alguno que tiene 0 o 1 amiga, entonces se setea un flag que indica que no existe ciclo de hamilton (recordemos que le problema indica que n=>3) Al mismo tiempo, se va verificando que los grados de cada vertice sea mayor o igual a n/2. Si para todo nodo se cumple, entonces valen las hipotesis del teorema de Dirac y se puede asegurar que existe un ciclo sin tener que encontrarlo. Me falta verificar las hipotesis del teorema de Ore, pero eso se realiza en el metodo esHamilton() ya que necesito haber cargado todos los nodos previamente. Las verificaciones anteriores son O(n), mientras que la del segundo teorema calculo que va a ser O(n^2) ya que tengo que verificar todos los pares de nodos no adyacentes.

--- tp1/ej2/src/graph.cpp
+++ tp1/ej2/src/graph.cpp
@@ -35,14 +35,35 @@ class Nodo{
 };
 
 Grafo::Grafo(map<int, list<int> > nodos_p){
+    cicloDeHamilton = NO_SE_SABE;
+    
     this->size = nodos_p.size();
     this->nodos = new Nodo[nodos_p.size()];
+    
+    //Empezamos suponiendo que se cumple la hipotesis
+    bool hipotesisDirac = true;
+    
     map<int, list<int> >::iterator it = nodos_p.begin();
     while(it != nodos_p.end()){
         assert(it->first > 0);
         this->nodos[it->first - 1] = Nodo(it->first, it->second);
         print(this->nodos[it->first - 1].toString());
+        
+        //Detectamos nodos con grado menor que 2
+        int gradoDeNodo = (int)this->nodos[it->first - 1].links.size();
+        if(gradoDeNodo < 2){
+            cicloDeHamilton = NO_EXISTE;
+        }
+        
+        if(gradoDeNodo < (size/2 + size%2)){
+            hipotesisDirac = false;
+        }
         it++;
+    }
+    
+    //Veamos si se cumplen las condiciones del teorema de Dirac
+    if(size >= 3 && hipotesisDirac){
+        cicloDeHamilton = EXISTE;
     }
 }
 
@@ -56,7 +77,22 @@ Nodo &Grafo::getNodo(nodo_id id){
 }
         
 bool Grafo::esHamilton() {
-    assert(size > 1);
+    assert(size > 2);
+
+    //Si ya se que hay o no un ciclo, devuelvo la respuesta sin intentar encontrar el ciclo
+    
+    if(cicloDeHamilton == EXISTE){
+        return true;
+    }
+    else if(cicloDeHamilton == NO_EXISTE){
+        return false;
+    }
+    
+    /** OPTIMIZACION - Probar si se cumplen las hipotesis del teorema de Ore (1960) **/
+        
+    /************************************************/
+
+
     //camino que hice a cada momento.
     stack<Nodo*> path;
     //ultimo nodo que visite en cada instancia del camino
@@ -66,14 +102,12 @@ bool Grafo::esHamilton() {
     //proximo nodo.
     it_list amiga;
     //nodo inicial
-    Nodo* inicio;
-    
-    inicio = &getNodo(1);
+    Nodo* inicio = &getNodo(1);;
     path.push(inicio);
     /*for(int i = 0; i < this->size; i++)
         visitadas[i] = false;*/
     inicio->visitada = true;
-    print("Visito 1");
+    //print("Visito 1");
     restantes[0] = inicio->links.begin();
     while(path.size() > 0){
         Nodo * actual = path.top();
@@ -86,7 +120,7 @@ bool Grafo::esHamilton() {
             //apilar nodo.
             actual = &getNodo(*amiga);
             amiga = actual->links.begin();
-            print("Visito " << actual->id);
+            //print("Visito " << actual->id);
             actual->visitada = true;
             restantes[path.size()] = amiga;
             path.push(actual);
@@ -98,7 +132,7 @@ bool Grafo::esHamilton() {
             //desapilo/avanzo  nodos
             if(actual->links.empty() || amiga == actual->links.end()){
                 //si consumi este nodo lo desapilo
-                print("Desvisito " << actual->id);
+                //print("Desvisito " << actual->id);
                 actual->visitada = false;
                 path.pop();
                 if(!path.empty()){
@@ -128,3 +162,61 @@ bool Grafo::esHamilton() {
     //delete [] visitadas;
     return false;
 }
+
+/*
+void Grafo::sortNodos(Nodo* v, int inicio, int fin){
+    if(fin - inicio < 2){
+        //swapeo los elementos de acuerdo a quien es mas grande
+        if( v[inicio].links.size() > v[fin].links.size()){
+            //swap
+            Nodo aux    = v[inicio];
+            v[inicio]   = v[fin];
+            v[fin]      = aux;
+        }
+    }
+    else{
+        int mitad = (inicio + fin)/2;
+        sortNodos(v, inicio, mitad);
+        sortNodos(v, mitad+1, fin);
+        
+        //merge
+        Nodo* sortedVector = new Nodo[fin-inicio+1];
+        int i = inicio;
+        int j = mitad+1;
+        int k = 0;
+        while(i<= mitad && j<=fin){
+            if(v[i].links.size() < v[j].links.size()){
+                sortedVector[k] = v[i];
+                i++;
+            }
+            else{
+                sortedVector[k] = v[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while(i <= mitad){
+            sortedVector[k] = v[i];
+            i++;
+            k++;
+        }
+     
+        while(j <= fin){
+            sortedVector[k] = v[j];
+            j++;
+            k++;
+        }
+ 
+        //restauramos v
+        i = inicio;
+        k = 0;
+        while( i <= fin ){
+            v[i] = sortedVector[k];
+            i++;
+            k++;
+        }
+        
+        delete[] sortedVector;
+    }
+}*/
