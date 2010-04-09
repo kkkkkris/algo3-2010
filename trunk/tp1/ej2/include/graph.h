3a9bcd3a2894cafd87897de535badfa06bd5b0ea
santiago.pivetta <santiago.pivetta@002c8353-63d8-11a8-4f12-8c9ae8348763>
2010-04-09 19:58:43 +0000

    Agregue un par de chequeos para contestar el problema sin tener que recorrer el grafo y armar el ciclo. Ambos chequeos se realizan en el constructor. Mientras se carga cada nodo en la estructura, se verifica por un lado que todos tengan mas de 1 amiga. Si existe alguno que tiene 0 o 1 amiga, entonces se setea un flag que indica que no existe ciclo de hamilton (recordemos que le problema indica que n=>3) Al mismo tiempo, se va verificando que los grados de cada vertice sea mayor o igual a n/2. Si para todo nodo se cumple, entonces valen las hipotesis del teorema de Dirac y se puede asegurar que existe un ciclo sin tener que encontrarlo. Me falta verificar las hipotesis del teorema de Ore, pero eso se realiza en el metodo esHamilton() ya que necesito haber cargado todos los nodos previamente. Las verificaciones anteriores son O(n), mientras que la del segundo teorema calculo que va a ser O(n^2) ya que tengo que verificar todos los pares de nodos no adyacentes.

--- trunk/tp1/ej2/include/graph.h
+++ trunk/tp1/ej2/include/graph.h
@@ -12,10 +12,15 @@ class Grafo {
         bool esHamilton();
         ~Grafo();
     private:
-        Nodo & getNodo(int nodo_id);
-        Nodo * nodos;
+        Nodo& getNodo(int nodo_id);
+        Nodo* nodos;
         int size;
-    
+        //cicloAsegurado indica si es necesario encontrar el ciclo para responder a la pregunta de su existencia, o ya fue probada por algun teorema
+        enum proofState {EXISTE,NO_EXISTE, NO_SE_SABE} cicloDeHamilton;
+        
+        //Ordena el array de nodos de acuerdo a su cantidad de links con mergesort
+        //void sortNodos(Nodo* v, int inicio, int fin);
+        list<int> getNodosNoAdyacentes(int nodoId);
 };
 
 
