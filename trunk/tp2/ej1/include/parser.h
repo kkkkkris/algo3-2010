cc5f1ca84222f72fbed5dcbb4fad3f54c4941e42
joaquin.garavaglia <joaquin.garavaglia@002c8353-63d8-11a8-4f12-8c9ae8348763>
2010-04-25 23:34:18 +0000

    subo cambios

--- trunk/tp2/ej1/include/parser.h
+++ trunk/tp2/ej1/include/parser.h
@@ -1,8 +1,18 @@
 #ifndef __PARSER__
 #define __PARSER__
-#include <list>
 
+#include <list>
+#include <string>
+
+#define Table int**
+#define number 0
+#define long_up 1
+#define pred_up 2
+#define long_down 3
+#define pred_down 4
 using namespace std;
 
-
+list<Table> parsearInstancias(string entrada);
+void list_toS(list<Table> tablas);
+void destructor(Table t);
 #endif //__PARSER__
