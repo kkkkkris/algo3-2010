1f7890be070f835ad192aac776add7ee1052df29
matiasbarbeito <matiasbarbeito@002c8353-63d8-11a8-4f12-8c9ae8348763>
2010-05-03 03:56:38 +0000

    Terminado el código del ej3. Funcionando.

--- trunk/tp2/ej3/src/bfs.cpp
+++ trunk/tp2/ej3/src/bfs.cpp
@@ -21,12 +21,32 @@ int main(int argc, char* argv[]) {
     }
     const char* entrada = argv[1];
     const char* salida = argv[2];
+    cout << "Leyendo el archivo de entrada ..." << endl;
     string contenido = leerArchivo(entrada);
+    cout << "... archivo leido exitosamente." << endl;
+    cout << "Parseando el archivo de entrada ..." << endl;
+    list<Grafo*> instancias = parsearInstancias(contenido);
+    cout << "... el parseo termino exitosamente." << endl;
     
+    cout << "Comenzando a procesar las instancias ..." << endl;
+    contenido = "";
+    for(list<Grafo*>::iterator it = instancias.begin(); it != instancias.end(); it++){
+        if((*it)->bfsMejorado()){
+            contenido += "libre\n";
+            cout << "    libre" << endl;
+        }
+        else{
+            contenido += "no\n";
+            cout << "    no" << endl;
+        }
+    }
+    cout << "... todas las instancias se procesaron exitosamente." << endl;
     
+    cout << "Escribiendo el archivo de salida ..." << endl;
     if(escribirArchivo(salida, contenido)){
         print("error escribiendo archivo");
         return 1;
     }
+    cout << "El archivo se ha escrito exitosamente ..." << endl;
     return 0;
 }
--- trunk/tp2/ej3/src/graph.cpp
+++ trunk/tp2/ej3/src/graph.cpp
@@ -12,30 +12,32 @@
 
 typedef list<int>::iterator it_list;
 
+enum Estado {INDEFINIDO, ABIERTA, CERRADA};
+
 using namespace std;
 class Nodo{
     public:
         Nodo(){};
-        Nodo(const int id, list<int> links, int llave, bool puerta):links(links), id(id), puerta(puerta), llave(llave) {};
+        Nodo(const int id, list<int>* links, int llave, bool puerta):links(links), id(id), llave(llave), puerta(puerta) {};
         string toString() {
             stringstream ss;
             ss << "nodo #" << id << " [";
-            for(it_list it = links.begin(); it != links.end(); it++) {
-                if(it != links.begin())
+            for(it_list it = links->begin(); it != links->end(); it++) {
+                if(it != links->begin())
                     ss << ", ";
                     ss << *it;
             }
             ss << "]";
             return ss.str();
         }
-        list<int> links;
+        list<int>* links;
         int id;
         // si es distinta de -1 dice el id de la puerta que abre
         int llave;
         bool puerta;
 };
 
-Grafo::Grafo(int n, int m, int p, list<int>[] nodos, int[] llaves, bool[] puertas){
+Grafo::Grafo(int n, int m, int p, list<int>* nodos[], int llaves[], bool puertas[]){
     this->n = n;
     this->m = m;
     this->p = p;
@@ -43,49 +45,68 @@ Grafo::Grafo(int n, int m, int p, list<int>[] nodos, int[] llaves, bool[] puerta
     
     for(int i = 0; i < n; i++){
         this->nodos[i] = new Nodo(i+1, nodos[i], llaves[i], puertas[i]);
-        print(this->nodos[i].toString());
     }
 
 }
 
-bool bfsMejorado(){
+bool Grafo::bfsMejorado(){
+    cout << "        n = " << this->n << ", m = " << this->m << ", p = " << this->p << endl;
+    for(int i = 0; i < n; i++){
+        cout << "        " << (this->nodos[i])->toString() << endl;
+    }
+    
     bool marcados[this->n];
+    Estado puertas[this->n];
     int i;
     Nodo* nodo;
     for(i = 0; i < n; i++){
         marcados[i] = false;
+        puertas[i] = INDEFINIDO;
     }
     queue<int> cola;
 
     marcados[0] = true;  
     cola.push(0);
     while (!cola.empty()){
-        nodo = nodos[cola.pop()];
-        for(it_list it = (nodo->links).begin(); it != (nodo->links).end(); it++){
-           if (!marcados[(nodo->id)-1]){
-                marcados[(nodo->id)-1]) = true;
-                cola.push((nodo->id)-1);
-           }
+        nodo = (this->nodos)[cola.front()];
+        cola.pop();
+        cout << "        Habitacion " << nodo->id << endl;
+        // si hay una puerta cerrada
+        if((nodo->puerta) && (puertas[(nodo->id)-1] != ABIERTA)){
+            puertas[(nodo->id)-1] = CERRADA;
+            cout << "        Puerta " << nodo->id << " cerrada" << endl;
+        }
+        // no hay puerta o ya está abierta
+        else{
+            // si hay llave
+            if((nodo->llave) != -1){
+                if(puertas[(nodo->llave)-1] == CERRADA){
+                    cola.push((nodo->llave)-1);
+                }
+                puertas[(nodo->llave)-1] = ABIERTA;
+                cout << "        Puerta " << nodo->llave << " abierta" << endl;
+            }
+            
+            Nodo* aux = NULL;
+            for(it_list it = (nodo->links)->begin(); it != (nodo->links)->end(); it++){
+                aux = (this->nodos)[(*it)-1];
+                // si llego a la salida devuelvo true
+                if(aux->id == this->n){return true;}
+                if(!marcados[(aux->id)-1]){
+                    marcados[(aux->id)-1] = true;
+                    cola.push((aux->id)-1);
+                }
+            }
         }
     }
 
+    // en este caso no pasé por la salida
     return false;
 }
 
 Grafo::~Grafo() {
-    delete [] nodos;
+    for(int i = 0; i < n; i++){
+        delete this->nodos[i];
+    }
+    delete [] (this->nodos);
 }
-
-Nodo* Grafo::getNodo(nodo_id id){
-    assert(id <= this->n && id > 0);
-    return this->nodos[id-1];
-}
-
-/*
-int Grafo::puertaAbierta(int llave){
-    assert(id <= this->n && id > 0);
-    return this->llaves[llave];
-}
-*/
-
-
