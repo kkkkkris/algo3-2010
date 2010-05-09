de6e091e3863053ca0b73f9b58ca8c9012f58beb
matiasbarbeito <matiasbarbeito@002c8353-63d8-11a8-4f12-8c9ae8348763>
2010-05-09 20:49:35 +0000

    Agregado el timer y la opción para medir tiempos más pequeños cambios en la intro y explicación.

--- tp2/ej3/src/bfs.cpp
+++ tp2/ej3/src/bfs.cpp
@@ -4,23 +4,29 @@
 #include <cstdlib>
 #include "parser.h"
 #include "file_utils.h"
+#include "timer.h"
 
 #define print(a) cout << a << endl
 
 using namespace std;
 
-string usage = "uso:  ./bfs entrada salida";
+string usage = "uso:  ./bfs entrada salida [tiempos]";
 
 
 int main(int argc, char* argv[]) {
     
-    if(argc < 3){
-        print("bad arguments");
-        print(usage);
+    bool calcularTiempo = false;
+    if (argc < 3 || argc > 4) {      
+        cerr << "bad arguments" << endl;
+        cerr << usage << endl;
         return 1;
     }
+    if (argc == 4) {calcularTiempo = true;}
+    
+    Timer timer;
     const char* entrada = argv[1];
     const char* salida = argv[2];
+    bool libre;
     cout << "Leyendo el archivo de entrada ..." << endl;
     string contenido = leerArchivo(entrada);
     cout << "... archivo leido exitosamente." << endl;
@@ -31,7 +37,16 @@ int main(int argc, char* argv[]) {
     cout << "Comenzando a procesar las instancias ..." << endl;
     contenido = "";
     for(list<Grafo*>::iterator it = instancias.begin(); it != instancias.end(); it++){
-        if((*it)->bfsMejorado()){
+        if (calcularTiempo) {
+            timer.nueva();
+            timer.empezar();
+        }
+        libre = (*it)->bfsMejorado();
+        if (calcularTiempo) {
+            timer.terminar();
+        }
+        
+        if(libre){
             contenido += "libre\n";
             cout << "    libre" << endl;
         }
@@ -48,5 +63,15 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     cout << "El archivo se ha escrito exitosamente ..." << endl;
+    
+    if (calcularTiempo) {
+        cout << "Escribiendo los tiempos de ejecucion en el archivo correspondiente ..." << endl;
+        if(escribirArchivo(argv[3], timer.tiempos())){
+            cout << "ERROR: hubo un problema al escribir los tiempos de ejecución." << endl;
+            return 1;
+        }
+        cout << "El archivo con los tiempos de ejecucion se ha escrito exitosamente ..." << endl;
+    }
+    
     return 0;
 }
