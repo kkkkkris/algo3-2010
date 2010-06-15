4cbc884bba57196fb77d9bfe877c89d748752f3b
noinflection <noinflection@002c8353-63d8-11a8-4f12-8c9ae8348763>
2010-06-15 02:29:57 +0000

    cambio de nombres

--- tp3/tabu/include/file_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __FILE_UTILS__
-#define __FILE_UTILS__
-using namespace std;
-
-string leerArchivo(const char* archivo);
-bool escribirArchivo(const char* archivo, string contenido);
-
-#endif
