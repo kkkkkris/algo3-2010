1269a6aa6313fd2130507da1ff4d3d950cf3f204
noinflection <noinflection@002c8353-63d8-11a8-4f12-8c9ae8348763>
2010-06-21 17:49:48 +0000

    emprolijado codigo.

--- tp3/metaheuristicas/include/graph.h
+++ tp3/metaheuristicas/include/graph.h
@@ -22,12 +22,12 @@ class Grafo {
         //metodos privados
         Nodo& getNodo(int nodo_id);
         bool sonAdyacentes(int i, int j);
-        int getGrado(int nodo_id);
         int getDensidad(int nodo_id);
-        void setDensidad(int nodo_id,int densidad);
-        bool esClique(int nodo_id,set<int> Cq);
-        set<int>* getVecinos(int nodo_id);
+        int setDensidad(int nodo_id);
+        Nodo& generarDensidad();
+        bool esClique(int nodo_id, set<int> Cq);
         set<int>* HC();
+        set<int>* HL();
 };
 
 
