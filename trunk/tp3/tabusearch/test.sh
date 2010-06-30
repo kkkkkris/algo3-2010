1044cce4d666d7f0f5a9550e7334b35c5951cbf9
santiago.pivetta <santiago.pivetta@002c8353-63d8-11a8-4f12-8c9ae8348763>
2010-06-30 23:10:28 +0000

    sdad

--- trunk/tp3/tabusearch/test.sh
+++ trunk/tp3/tabusearch/test.sh
@@ -2,25 +2,28 @@
 
 MAX_ITER_LIST="5 10 20 30 40 50"
 MAX_TABU_LIST="20 50 100 200"
+ROOT="../cliques/entradas/"
 
 echo "iniciando testing..."
 
-for lt in $MAX_TABU_LIST
+for archivo in $(ls $ROOT)
 do
-    for k in $MAX_ITER_LIST
-    do
-        rm "tests_resultados"
-        touch "tests_resultados"
-        echo "modo p - maxIter: "$k" maxTabu: "$lt
-        ./ts $k p $lt >> "tests_resultados"
-    done
+	rm $ROOT$archivo".test"
+	touch $ROOT$archivo".test"
+	
+	for lt in $MAX_TABU_LIST
+	do
+		#for k in $MAX_ITER_LIST
+		#do		
+		#	echo "archivo: "$ROOT$archivo" modo p maxIter: "$k" maxTabu: "$lt
+		#	./ts $ROOT$archivo out $k p $lt >> $ROOT$archivo".test"
+		#done
 
-    for k in $MAX_ITER_LIST
-    do
-        rm "tests_resultados"
-        touch "tests_resultados"
-        echo "modo s - maxIter: "$k" maxTabu: "$lt
-        ./ts $k s $lt >> "tests_resultados"
-    done
+		for k in $MAX_ITER_LIST
+		do
+			echo "archivo: "$ROOT$archivo" modo s maxIter: "$k" maxTabu: "$lt
+			./ts $ROOT$archivo out $k s $lt >> $ROOT$archivo".test"
+		done
+	done
 done
 echo "testing finalizando!"
